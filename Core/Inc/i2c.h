7c395eccb079e40099f76f278f55f245bb8ea1d8
BogdanKuljanin <kuljaninbogdan@gmail.com>
2026-04-28 23:50:35 +0200

    setting up project

---
+++ Core/Inc/i2c.h
@@ -0,0 +1,17 @@
+#ifndef INC_I2C_H_
+#define INC_I2C_H_
+#include "stm32f4xx_hal.h"
+
+#define AS5600_ADDRESS (0x36)		// AS5600 I2C address
+
+#define AS5600_CONF_1_REG (0x07) 	// Configuration register 1
+
+#define AS5600_PWM_MODE (0x20)	 	// PWM mode of the encoder
+#define AS5600_PWM_920_HZ (0x80)	// PWM frequency 920 Hz
+
+I2C_HandleTypeDef hi2c1;
+
+void as5600_init(void);
+void MX_I2C1_Init(void);
+
+#endif /* INC_I2C_H_ */
---
+++ Core/Src/i2c.c
@@ -0,0 +1,28 @@
+#include "stm32f4xx_hal.h"
+#include "i2c.h"
+
+void as5600_init(void)
+{
+	HAL_I2C_Mem_Write(&hi2c1, AS5600_ADDRESS << 1, AS5600_CONF_1_REG, I2C_MEMADD_SIZE_8BIT,
+			(AS5600_PWM_MODE | AS5600_PWM_920_HZ), 1, 100);
+}
+
+void MX_I2C1_Init(void)
+{
+
+  hi2c1.Instance = I2C1;
+  hi2c1.Init.ClockSpeed = 100000;
+  hi2c1.Init.DutyCycle = I2C_DUTYCYCLE_2;
+  hi2c1.Init.OwnAddress1 = 0;
+  hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c1.Init.OwnAddress2 = 0;
+  hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hi2c1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+
+}
