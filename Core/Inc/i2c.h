7d2a0cd97a86816d775f68f74a9aa7864a791eec
BogdanKuljanin <kuljaninbogdan@gmail.com>
2026-04-30 00:15:01 +0200

    fixing config register address and value

--- Core/Inc/i2c.h
+++ Core/Inc/i2c.h
@@ -4,10 +4,10 @@
 
 #define AS5600_ADDRESS (0x36)		// AS5600 I2C address
 
-#define AS5600_CONF_1_REG (0x07) 	// Configuration register 1
+#define AS5600_CONF_1_REG (0x08) 	// Configuration register 2
 
 #define AS5600_PWM_MODE (0x20)	 	// PWM mode of the encoder
-#define AS5600_PWM_920_HZ (0x80)	// PWM frequency 920 Hz
+#define AS5600_PWM_920_HZ (0xC0)	// PWM frequency 920 Hz
 
 extern I2C_HandleTypeDef hi2c1;
 
