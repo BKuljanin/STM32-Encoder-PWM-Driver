adbb717822c1c2f34c10d921be5220424b36eae6
BogdanKuljanin <kuljaninbogdan@gmail.com>
2026-05-01 13:27:53 +0200

    handling wrapping angle to prevent spikes between ~360 and ~0

--- Core/Src/as5600.c
+++ Core/Src/as5600.c
@@ -15,14 +15,30 @@ float lp_filter(float signal, float LP_cutoff, float delta_t)
 void as5600_pwm_to_angle(void)
 {
 	encoder.angle_previous = encoder.angle;
-	encoder.angle = ((float)pulse_width / (float)pulse_period) * 360.0f;
+
+	uint32_t period = pulse_period;
+	if (period < 900) period = 900;
+
+	float angle = ((float)pulse_width / (float)period) * 360.0f;
+
+	if (angle > 360.0f) angle = 360.0f;
+	if (angle < 0.0f) angle = 0.0f;
+
+	encoder.angle = angle;
 }
 
 void as5600_calculate_speed(void)
 {
-	// Raw angular speed calculation, current - previous angle
-	float angular_speed_raw = (encoder.angle - encoder.angle_previous) / ((float)pulse_period / 1e6f);	// Converting pulse width from us to s to get [deg/s]
+	uint32_t period = pulse_period;
+	if (period < 900) period = 900;
+
+	// Wrap aware delta, if prev ~360 and current ~0 (or vice versa) compute actual small movement
+	float delta_angle = encoder.angle - encoder.angle_previous;
+	if (delta_angle > 180.0f) delta_angle -= 360.0f;
+	if (delta_angle < -180.0f) delta_angle += 360.0f;
+
+	float angular_speed_raw = delta_angle / ((float)period / 1e6f);	// Converting pulse width from us to s to get [deg/s]
 
 	// Applying LP filter to raw speed in order to filter amplified noise from encoder (amplified by derivation)
-	encoder.angular_speed = lp_filter(angular_speed_raw, SPEED_LP_CUTOFF, (float)pulse_period / 1e6f);
+	encoder.angular_speed = lp_filter(angular_speed_raw, SPEED_LP_CUTOFF, (float)period / 1e6f);
 }
